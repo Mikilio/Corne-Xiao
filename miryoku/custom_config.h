d46355d5df1ccbd913bc1c5d7e7a68ab38dd1081
Mikilio <official.mikilio@gmail.com>
2023-12-23 08:36:44 +0100

    try cross hand

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,4 +1,9 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define U_TAPPING_TERM 500
+#define MIRYOKU_HOME_MOD_CROSS_HAND
+#define MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH \
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
+LHM LGUI A,        LHM LALT R,        LHM LCTRL S,       LHM LSHFT T,       &kp G,             &kp M,             RHM LSHFT N,       RHM LCTRL E,       RHM LALT I,        RHM LGUI O,        \
+U_LT(U_BUTTON, Z),    LHM RALT X,        &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         RHM RALT DOT,    U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -4,4 +4,16 @@
 #pragma once
 
 #define U_MT(MOD, TAP) &u_mt MOD TAP
+#if defined(MIRYOKU_HOME_MOD_RIGHT_POSITIONS)
+#define LHM &lhm
+#else
+#define LHM &hm
+#endif
+
+#if defined(MIRYOKU_HOME_MOD_LEFT_POSITIONS)
+#define RHM &rhm
+#else
+#define RHM &hm
+#endif
+
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
