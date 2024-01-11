80f40cc62b74fd801c11de6db03cddaba76f7101
Mikilio <official.mikilio@gmail.com>
2024-01-11 23:42:32 +0100

    feat: adding gaming layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,3 +2,9 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_HOME_MOD_CROSS_HAND
+#define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
+#define MIRYOKU_LAYER_GAME \
+&kp ESC,          &kp N1,            &kp N2,            &kp N3,            &kp N4,            U_NA,              U_NA,          U_NA,          U_NA,          U_NA, \
+&kp TAB,          &kp Q,             &kp W,             &kp E,             &kp R,             U_NA,              U_NA,          U_NA,          U_NA,          U_NA, \
+&kp LSHFT,        &kp A,             &kp S,             &kp D,             &kp F,             U_NA,              U_NA,          U_NA,          U_NA,          U_NA, \
+U_NP,              U_NP,             &kp LCTRL,         &kp LALT,          &kp SPACE,         &u_to_U_BASE,      U_NA,          U_NA,          U_NP,          U_NP
