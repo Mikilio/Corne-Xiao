caacb387eb9096548188b833e86690c2a3fb159c
Mikilio <official.mikilio@gmail.com>
2024-01-13 12:25:08 +0100

    feat: quality of life changes
    - moved button layer to right hand side aux layers
    - made tap layer accessible only from game layer
    - added clipboard buttons

--- miryoku/miryoku_babel/miryoku_layer_list.h
+++ miryoku/miryoku_babel/miryoku_layer_list.h
@@ -10,7 +10,7 @@
   MIRYOKU_X(BASE, "Base")                                                      \
   MIRYOKU_X(EXTRA, "Extra")                                                    \
   MIRYOKU_X(GAME, "Game")                                                      \
-  MIRYOKU_X(BUTTON, "Button")                                                  \
+  MIRYOKU_X(TAP, "Tap")                                                        \
   MIRYOKU_X(NAV, "Nav")                                                        \
   MIRYOKU_X(MOUSE, "Mouse")                                                    \
   MIRYOKU_X(MEDIA, "Media")                                                    \
@@ -21,7 +21,7 @@
 #define U_BASE 0
 #define U_EXTRA 1
 #define U_GAME 2
-#define U_BUTTON 3
+#define U_TAP 3
 #define U_NAV 4
 #define U_MOUSE 5
 #define U_MEDIA 6
